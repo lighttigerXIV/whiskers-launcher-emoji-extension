076262303d00d0c7b4f7b649999df96d92e23c31
lighttigerxiv <lighttigerxiv@gmail.com>
2024-06-05 22:16:28 +0100

    better emojis library :p

--- src/results/mod.rs
+++ src/results/mod.rs
@@ -1,39 +1,34 @@
+use convert_case::{Case, Casing};
+use fuzzy_matcher::{skim::SkimMatcherV2, FuzzyMatcher};
 use whiskers_launcher_rs::{
     action::{Action, CopyAction},
     api::extensions::{send_response, ExtensionRequest},
-    result::{TextResult, WLResult},
-    utils::get_search,
+    result::{TextResult, WLResult}
 };
 
-struct EmojiResult {
-    pub glyph: String,
-    pub name: String,
-}
-
 pub fn handle_results(request: ExtensionRequest) {
-    let search = get_search(request.search_text.unwrap());
+    let search = request.search_text.unwrap();
 
-    if search.search_text.is_empty() {
+    if search.is_empty() {
         send_response(Vec::new());
     }
 
     let mut results = Vec::<WLResult>::new();
+    let matcher = SkimMatcherV2::default();
 
-    let emojis = emoji::search::search_annotation(&search.search_text, "en")
-        .into_iter()
-        .map(|e| EmojiResult {
-            glyph: e.glyph.to_owned(),
-            name: e.name.to_owned(),
-        });
+    for emoji in emojis::iter() {
+        if matcher
+            .fuzzy_match( emoji.name() , &search)
+            .is_some()
+        {
+            let action = Action::new_copy(CopyAction::new(emoji.as_str()));
+            let result = WLResult::new_text(TextResult::new(
+                format!("{} - {}", emoji.to_string(), emoji.name().to_case(Case::Title)),
+                action,
+            ));
 
-    for emoji in emojis {
-        let action = Action::new_copy(CopyAction::new(&emoji.glyph));
-        let result = WLResult::new_text(TextResult::new(
-            format!("{} - {}", &emoji.glyph, &emoji.name),
-            action,
-        ));
-
-        results.push(result);
+            results.push(result);
+        }
     }
 
     send_response(results);
