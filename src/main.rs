00f79adb8a834809f0d47e82f3ce5543bfdf542a
lighttigerxiv <lighttigerxiv@gmail.com>
2024-06-26 12:34:58 +0100

    add missing windows rule

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+//REQUIRED FOR WINDOWS EXTENSIONS
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 use results::handle_results;
 use whiskers_launcher_rs::api::extensions::get_extension_request;
 
