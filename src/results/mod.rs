34e7277175eee6c3b2e53d1fa22fb5c366de1c9a
lighttigerxiv <lighttigerxiv@gmail.com>
2024-09-30 21:39:29 +0100

    🛠️ Use sniffer instead of fuzzy matcher

--- src/results/mod.rs
+++ src/results/mod.rs
@@ -1,29 +1,30 @@
 use convert_case::{Case, Casing};
-use fuzzy_matcher::{skim::SkimMatcherV2, FuzzyMatcher};
+use sniffer_rs::sniffer::Sniffer;
 use whiskers_launcher_rs::{
     action::{Action, CopyAction},
     api::extensions::{send_response, ExtensionRequest},
-    result::{TextResult, WLResult}
+    result::{TextResult, WLResult},
 };
 
 pub fn handle_results(request: ExtensionRequest) {
     let search = request.search_text.unwrap();
 
-    if search.is_empty() {
+    if search.trim().is_empty() {
         send_response(Vec::new());
     }
 
     let mut results = Vec::<WLResult>::new();
-    let matcher = SkimMatcherV2::default();
+    let sniffer = Sniffer::new();
 
     for emoji in emojis::iter() {
-        if matcher
-            .fuzzy_match( emoji.name() , &search)
-            .is_some()
-        {
+        if sniffer.clone().matches(emoji.name(), &search) {
             let action = Action::new_copy(CopyAction::new(emoji.as_str()));
             let result = WLResult::new_text(TextResult::new(
-                format!("{} - {}", emoji.to_string(), emoji.name().to_case(Case::Title)),
+                format!(
+                    "{} - {}",
+                    emoji.to_string(),
+                    emoji.name().to_case(Case::Title)
+                ),
                 action,
             ));
 
